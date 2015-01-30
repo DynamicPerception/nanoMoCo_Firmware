32dd1eb123afee7518885fd502ced74db98f5acf
mploof <michael@dynamicperception.com>
2015-01-30 16:42:50 -0500

    Increment firmware version

--- Firmware/Motion_Engine/Motion_Engine.ino
+++ Firmware/Motion_Engine/Motion_Engine.ino
@@ -47,7 +47,7 @@ See dynamicperception.com for more information
 const char SERIAL_TYPE[] = "OMAXISVX";
 
   // serial api version
-const int SERIAL_VERSION = 23;
+const int SERIAL_VERSION = 24;
 
   // # of flashes of debug led at startup
 const byte START_FLASH_CNT = 5;
