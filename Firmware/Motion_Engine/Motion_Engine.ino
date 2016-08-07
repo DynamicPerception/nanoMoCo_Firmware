392077d448eb146133af78ae866257b8807f9d65
Delrious <tremont@gmail.com>
2016-08-06 18:17:03 -0700

    another missed update

--- Firmware/Motion_Engine/Motion_Engine.ino
+++ Firmware/Motion_Engine/Motion_Engine.ino
@@ -467,7 +467,7 @@ void setup() {
 		motor[i].programBackCheck(false);	 
 	}
         // Leaving this here for now, remove before merging
-        debug.setState(DebugClass::DB_FUNCT | DebugClass::DB_COM_OUT | DebugClass::DB_GEN_SER |  DebugClass::DB_MOTOR);
+        //debug.setState(DebugClass::DB_FUNCT | DebugClass::DB_COM_OUT | DebugClass::DB_GEN_SER |  DebugClass::DB_MOTOR);
           
         // Initialize USB Controller Mode
         USBCtrlrUI.init();
